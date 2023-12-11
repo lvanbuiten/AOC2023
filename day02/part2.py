d72fa9e51ea036480f2960dfd268c533004e94a2
Lex van Buiten <L.vanBuiten@sigmax.nl>
2023-12-11 16:07:39 +0100

    Day 2 solution

---
+++ day02/part2.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+from __future__ import absolute_import
+
+import argparse
+import os
+import sys
+import string
+import collections
+import math
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from support import support
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    total = 0
+    lines = s.splitlines()
+    for line in lines:
+        pick_min = {'red': 0,'green': 0,'blue': 0}
+        picksStr = line.split(': ')[1]
+        for pick in picksStr.replace(';', ',').split(', '):
+            cnt, color = pick.split(' ')
+            pick_min[color] = max(pick_min[color], int(cnt))
+            
+        total += math.prod(pick_min.values())
+    return total
+
+
+INPUT_S = '''\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+'''
+EXPECTED = 2286
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
