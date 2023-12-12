78ff3a358ecabe31d61280bfa95362be28d1734c
Lex van Buiten <L.vanBuiten@sigmax.nl>
2023-12-12 23:26:25 +0100

    Day 4 solution

---
+++ day04/part1.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+from __future__ import absolute_import
+
+import argparse
+import os
+import sys
+import string
+import collections
+import math
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from support import support
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    total = 0
+    lines = s.splitlines()
+    for line in lines:
+        win_nrS,my_nrS = line.split(': ')[1].split(' | ')
+        win_nr = support.parse_numbers_split(win_nrS)
+        my_nr = support.parse_numbers_split(my_nrS)
+        amount = len(list(set(win_nr) & set(my_nr)))
+        total += (1 << amount-1) if amount > 0 else 0
+    return total
+
+
+INPUT_S = '''\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+'''
+EXPECTED = 13
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
